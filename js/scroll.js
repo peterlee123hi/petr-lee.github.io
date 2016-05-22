348f4c46d9ede9f47ed932a1f947f132fbd8b4ed
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-05-22 15:10:04 -0700

    Added portfolio

--- js/scroll.js
+++ js/scroll.js
@@ -2,6 +2,24 @@ $(document).ready(function(){
 	$('#about-link').click(function() {
 		$('html, body').animate({
 	        scrollTop: $('section.about').offset().top
-	    }, 600);
+	    }, 1000);
+	});
+
+	$('#design-link').click(function() {
+		$('html, body').animate({
+	        scrollTop: $('section#design-portfolio').offset().top
+	    }, 1000);
+	});
+
+	$('#web-link').click(function() {
+		$('html, body').animate({
+	        scrollTop: $('section#web-portfolio').offset().top
+	    }, 1400);
+	});
+
+	$('section.portfolio img').hover(function(){
+			$(this).stop().animate({"top" : "-10px"});
+		}, function(){
+	 		$(this).stop().animate({"top": "0"});
 	});
 });
