fa7f700fdb2822d2f83c9c38154fa5eb3ffa83e5
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-05-22 12:33:26 -0700

    Reset

--- js/applytyped.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	var str = "CS Theory + Practical Programming";
-	$(".typed").typed({
-	  strings: [str],
-	  typeSpeed: 0
-	});
-});
--- js/side-menu.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(".side-menu-toggle").click(function() {
-		$(".contain-side-menu").toggleClass("active");
-		$(".side-menu").toggleClass("active");
-		$(".side-menu-toggle").toggleClass("active");
-	});
-});
