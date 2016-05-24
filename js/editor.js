97b7b66b8bd9caca6d78d73efe66876189a8dd05
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-05-23 23:55:44 -0500

    Added templates

--- js/editor.js
+++ js/editor.js
@@ -1,10 +1,24 @@
 // Editor Settings (Provided by C9)
 var editor = ace.edit("editor");
+var base = 'http://petr-lee.github.io/docs/code/';
 editor.setTheme("ace/theme/monokai");
 editor.setShowPrintMargin(false);
 editor.session.setMode("ace/mode/c_cpp");
 
-jQuery.get('http://petr-lee.github.io/docs/code/main_lite.cpp', function(data) {
+$.get(base + 'main_lite.cpp', function(data) {
 	editor.setValue(data);
 	editor.clearSelection();
+});
+
+$(document).ready(function() {
+	$('li').click(function() {
+		if(!($(this).hasClass('selected'))) {
+			$('li.selected').removeClass('selected');
+			$(this).addClass('selected');
+			$.get(base + $(this).attr('file'), function(data) {
+				editor.session.setValue(data);
+				editor.clearSelection();
+			});
+		}
+	});
 });
