1a718bed18204871db0359466a7d3f2ebb50c369
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-05-24 00:09:44 -0500

    Added to Main.java

--- docs/code/Main.java
+++ docs/code/Main.java
@@ -2,8 +2,21 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	// Global variables here...
+
+	public static void main(String[] args) throws IOException {
+		System.exit(0);
+	}
+
+	public static void input() throws IOException {
+		Scanner in = new Scanner(System.in);
+	}
+
+	public static void solve() {
+
+	}
+
+	public static void output() {
+
 	}
 }
-
