3385b663749eca7e5959afe2aa2c4acb523053b9
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-07-19 11:34:23 +0200

    Added design portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,23 @@
 
   particlesJS.load('particles-js', 'assets/config/particles.json');
 
+  // NAVBAR LINKS
   $('a#about-link').click(function() {
     $('html, body').animate({
       scrollTop: $('.cards').offset().top - 64
     }, 500);
+  });
+
+  $('a#about-link').click(function() {
+    $('html, body').animate({
+      scrollTop: $('section#design').offset().top - 64
+    }, 500);
+  });
+
+  // PORTFOLIO ANIMATIONS
+  $('section.portfolio img').hover(function() {
+      $(this).stop().animate({"top" : "-10px"}, 250, 'swing');
+    }, function() {
+      $(this).stop().animate({"top": "0"}, 250, 'swing');
   });
 }(jQuery));
