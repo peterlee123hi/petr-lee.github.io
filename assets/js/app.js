a635e27bf7311fa8905c970ca882f10649b1c9c0
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-07-20 01:31:54 +0200

    Added a few descriptions

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,27 +28,84 @@
     }, 500);
   });
 
-  // PORTFOLIO ANIMATIONS
+  // PORTFOLIO INTERACTIVITY
   $('section.portfolio img').hover(function() {
       $(this).stop().animate({"top" : "-10px"}, 250, 'swing');
     }, function() {
       $(this).stop().animate({"top": "0"}, 250, 'swing');
   });
 
-  // Ok, this is annoying.
-  // Why won't the animations work?!?!
+  // DESIGN PORTFOLIO
   $('section#design img').click(function() {
     var img = $('<img>');
     img.attr('src', $(this).attr("src"));
     $('#overlay').append(img);
     $('#overlay').attr("opacity", 0);
     $('#overlay').attr("display", "block");
-    $('#overlay').fadeIn(250);
+    $('#overlay').fadeIn(0);
+  });
+
+  var codeDescriptions = {
+    "fzcf-link": {
+      "name": "Feng Zheng Cultural Foundation",
+      "info": "This was where it all began. A nonprofit organized by a friend asked me to make their website. I was new to the front-end web development community, and only used Bootstrap to make this website. Looking back at this site, I realized I've come a long way since then.",
+      "link": "http://fzculturalfoundation.org/",
+      "repo": "https://github.com/fzcf/fzcf.github.io"
+    },
+
+    "globaltoday-link": {
+      "name": "Global Today",
+      "info": "I made this website as a homework assignment for my english class about the environment.",
+      "link": "",
+      "repo": ""
+    },
+
+    "unrac-link": {
+      "name": "",
+      "info": "",
+      "link": "",
+      "repo": ""
+    },
+
+    "boilerplate-link": {
+      "name": "",
+      "info": "",
+      "link": "",
+      "repo": ""
+    }
+  };
+
+  // CODE PORTFOLIO
+  $('section#code img').click(function() {
+    var h1 = $('<h1>');
+    var p = $('<p>');
+    var img = $('<img>');
+    var link = $('<a class="waves-effect waves-light btn blue" target="_blank">');
+    var repo = $('<a class="waves-effect waves-light btn blue" target="_blank">');
+
+    h1.text(codeDescriptions[$(this).attr('id')]["name"]);
+    p.text(codeDescriptions[$(this).attr('id')]["info"]);
+    img.attr('src', $(this).attr("src"));
+    link.attr('href', codeDescriptions[$(this).attr('id')]["link"]);
+    repo.attr('href', codeDescriptions[$(this).attr('id')]["repo"]);
+    link.text("Visit");
+    repo.text("Github");
+
+    $('#overlay').append(h1);
+    $('#overlay').append(p);
+    $('#overlay').append(link);
+    $('#overlay').append(repo);
+    $('#overlay').append(img);
+    $('#overlay').attr("opacity", 0);
+    $('#overlay').attr("display", "block");
+    $('#overlay').fadeIn(0);
   });
 
   $('#overlay h6').click(function() {
-    $('#overlay').fadeOut(250, function() {
-      $('#overlay img').remove();
+    $('#overlay').fadeOut(0, function() {
+      var x = $('#overlay h6').detach();
+      $('#overlay').empty();
+      $('#overlay').append(x);
       $('#overlay').hide();
     });
   });
