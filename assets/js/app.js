45161d5b290c37a5eddd05c5c07ec874e0c403c1
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-07-19 11:05:39 +0200

    Refactored

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,10 @@
   $(".button-collapse").sideNav();
 
   particlesJS.load('particles-js', 'assets/config/particles.json');
+
+  $('a#about-link').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.cards').offset().top - 64
+    }, 500);
+  });
 }(jQuery));
