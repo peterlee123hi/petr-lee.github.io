b8c2c012ecbf81bad65b4bef0536439b4670e096
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-07-19 11:49:37 +0200

    Added buggy overlay

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,9 +10,15 @@
     }, 500);
   });
 
-  $('a#about-link').click(function() {
+  $('a#design-link').click(function() {
     $('html, body').animate({
-      scrollTop: $('section#design').offset().top - 64
+      scrollTop: $('section#design').offset().top
+    }, 500);
+  });
+
+  $('a#code-link').click(function() {
+    $('html, body').animate({
+      scrollTop: $('section#code').offset().top
     }, 500);
   });
 
@@ -21,5 +27,23 @@
       $(this).stop().animate({"top" : "-10px"}, 250, 'swing');
     }, function() {
       $(this).stop().animate({"top": "0"}, 250, 'swing');
+  });
+
+  // Ok, this is annoying.
+  // Why won't the animations work?!?!
+  $('.portfolio img').click(function(){
+    var img = $('<img>');
+    img.attr('src', $(this).attr("src"));
+    $('#overlay').append(img);
+    $('#overlay').attr("opacity", 0);
+    $('#overlay').attr("display", "block");
+    $('#overlay').fadeTo(100, 1);
+  });
+
+  $('#overlay h6').click(function() {
+    $('#overlay').fadeTo(100, 0, function() {
+      $('#overlay img').remove();
+      $('#overlay').hide();
+    });
   });
 }(jQuery));
