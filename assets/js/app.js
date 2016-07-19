26d80d8e3a46ecbb960eca5af2c46e9db142bbc8
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-07-19 13:13:01 +0200

    Finished v1.0.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-(function($){
+(function($) {
   $(".button-collapse").sideNav();
 
   particlesJS.load('particles-js', 'assets/config/particles.json');
---
+++ assets/js/editor.js
@@ -0,0 +1,23 @@
+(function($) {
+  var editor = ace.edit("editor");
+  var base = 'http://petr-lee.github.io/docs/code/';
+  editor.setTheme("ace/theme/monokai");
+  editor.setShowPrintMargin(false);
+  editor.session.setMode("ace/mode/c_cpp");
+
+  $.get(base + 'main_lite.cpp', function(data) {
+    editor.setValue(data);
+    editor.clearSelection();
+  });
+
+  $('li').click(function() {
+    if(!($(this).hasClass('selected'))) {
+      $('li.selected').removeClass('selected');
+      $(this).addClass('selected');
+      $.get(base + $(this).attr('file'), function(data) {
+        editor.session.setValue(data);
+        editor.clearSelection();
+      });
+    }
+  });
+}(jQuery));
