e40e2b15f0212f5fd4b185ba5bd67e33d3292a0d
PeterTheDestroyer <peter.lee@berkeley.edu>
2016-08-13 19:02:33 -0700

    Added Alary Language project

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,7 +90,7 @@
 
     "alarylanguage-link": {
       "name": "Alary Language",
-      "info": "I was requested to make this website for an official club at UC Berkeley. Alary Language focuses on connecting students who are passionate about learning and teaching languages. The club provides a more personal language learning experience with companionship.",
+      "info": "I was requested to make this website for an official club at UC Berkeley. Alary Language focuses on connecting students who are passionate about learning and teaching languages. The club provides a more personal language learning experience with companionship. This website was made in a single day.",
       "link": "http://peterlee.tech/AlaryLanguage",
       "repo": "https://github.com/petr-lee/AlaryLanguage"
     }
