f95f590e95a12af12ef36ad243749d205d281d6c
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-03-23 18:25:35 -0700

    Major style update

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@
 $(function () {
 	var currentLocation = 'intro';
 
-	var transition = function transition(pageID) {
-		$('.background').addClass('background-active');
+	var transition = function transition(event, pageID) {
+		$('.background').css('clip-path', 'circle(150vmax at 0px 0px)');
 
 		setTimeout(function () {
 			if (pageID === 'intro') {
@@ -16,69 +16,34 @@ $(function () {
 
 				$('section').addClass('hide');
 				$('section#' + pageID).removeClass('hide');
-
-				if (pageID === 'design') {
-					loadIsoGrids();
-				}
 			}
 			currentLocation = pageID;
 
-			$('.background').removeClass('background-active');
+			$('.background').css('clip-path', 'circle(0vmax at 0px 0px)');
 		}, 1500);
 	};
 
-	$('.intro-btn').click(function () {
-		transition('intro');
+	$('.intro-btn').click(function (e) {
+		transition(e, 'intro');
 	});
 
-	$('.code-btn').click(function () {
-		transition('code');
+	$('.code-btn').click(function (e) {
+		transition(e, 'code');
 	});
 
-	$('.web-btn').click(function () {
-		transition('web');
+	$('.web-btn').click(function (e) {
+		transition(e, 'web');
 	});
 
-	$('.design-btn').click(function () {
-		transition('design');
+	$('.design-btn').click(function (e) {
+		transition(e, 'design');
 	});
 
-	$('.theory-btn').click(function () {
-		transition('theory');
+	$('.theory-btn').click(function (e) {
+		transition(e, 'theory');
 	});
 
-	$('.music-btn').click(function () {
-		transition('music');
+	$('.music-btn').click(function (e) {
+		transition(e, 'music');
 	});
-
-	var loadIsoGrids = function loadIsoGrids() {
-		function getRandomInt(min, max) {
-			return Math.floor(Math.random() * (max - min + 1)) + min;
-		}
-
-		[].slice.call(document.querySelectorAll('.isolayer')).forEach(function (el) {
-			new IsoGrid(el, {
-				type: 'scrollable',
-				transform: 'translateX(-15vw) translateY(275px) rotateX(45deg) rotateZ(45deg)',
-				stackItemsAnimation: {
-					properties: function properties(pos) {
-						return {
-							translateZ: (pos + 1) * 50,
-							rotateZ: getRandomInt(-3, 3)
-						};
-					},
-					options: function options(pos, itemstotal) {
-						return {
-							type: dynamics.bezier,
-							duration: 500,
-							points: [{ 'x': 0, 'y': 0, 'cp': [{ 'x': 0.2, 'y': 1 }] }, { 'x': 1, 'y': 1, 'cp': [{ 'x': 0.3, 'y': 1 }] }]
-						};
-					}
-				},
-				onGridLoaded: function onGridLoaded() {
-					classie.add(document.body, 'grid-loaded');
-				}
-			});
-		});
-	};
 });
