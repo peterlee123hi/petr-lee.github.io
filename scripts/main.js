012263e7ba1426d0b285585eff45e81bf9c2e702
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-03-24 03:29:31 -0700

    Updated style

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@ $(function () {
 	}, 500);
 
 	var transition = function transition(event, pageID) {
+		$('.background').css('-webkit-clip-path', 'circle(150vmax at 0px 0px)');
 		$('.background').css('clip-path', 'circle(150vmax at 0px 0px)');
 
 		setTimeout(function () {
@@ -24,6 +25,7 @@ $(function () {
 			}
 			currentLocation = pageID;
 
+			$('.background').css('-webkit-clip-path', 'circle(0vmax at 0px 0px)');
 			$('.background').css('clip-path', 'circle(0vmax at 0px 0px)');
 		}, 1500);
 	};
@@ -51,4 +53,42 @@ $(function () {
 	$('.music-btn').click(function (e) {
 		transition(e, 'music');
 	});
+});
+'use strict';
+
+$(function () {
+	var showSlide = function showSlide(width, image) {
+		$('.intro-slide').css('top', '-20px');
+		$('.intro-slide').css('background-size', width + 'px auto');
+		$('.intro-slide').css('background-image', 'url("images/' + image + '")');
+		$('.intro-slide').animate({
+			'top': 0,
+			'opacity': 1
+		}, {
+			'duration': 500,
+			'easing': 'linear'
+		});
+		setTimeout(function () {
+			$('.intro-slide').animate({
+				'top': '20px',
+				'opacity': 0
+			}, {
+				'duration': 500,
+				'easing': 'linear'
+			});
+		}, 2000);
+	};
+
+	var index = 0;
+	var images = ['laptop.png', 'openark-logo.png', 'microsoft.png', 'illustrator.svg'];
+	var widths = [400, 400, 400, 160];
+
+	var runSlideshow = function runSlideshow() {
+		index = index % images.length;
+		showSlide(widths[index], images[index]);
+		index += 1;
+		setTimeout(runSlideshow, 3000);
+	};
+
+	runSlideshow();
 });
