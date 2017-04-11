061df872be0e93d3dba57f9e7c13eed7a815a1c5
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-04-11 10:44:24 -0700

    Decreased rotation

--- app/scripts/modal.js
+++ app/scripts/modal.js
@@ -5,8 +5,8 @@ $(function() {
 	    var halfH = (this.clientHeight / 2);
 	    var coorX = (halfW - (event.screenX - this.offsetLeft));
 	    var coorY = (halfH - (event.screenY - this.offsetTop));
-	    var degX  = ((coorY / halfH) * 6) + 'deg';
-	    var degY  = ((coorX / halfW) * -6) + 'deg';
+	    var degX  = ((coorY / halfH) * 3) + 'deg';
+	    var degY  = ((coorX / halfW) * -3) + 'deg';
 
 	    $(this).css('transform', function() {
 	      return 'perspective(600px) translate3d(0, -2px, 0) scale(1.1) rotateX('+ degX +') rotateY('+ degY +')';
