fa4499f4422dfc5cea6a581bd2a0bfcd88b67548
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-04-10 23:26:39 -0700

    Added cool modals

--- scripts/main.js
+++ scripts/main.js
@@ -1302,6 +1302,24 @@ $(function () {
 'use strict';
 
 $(function () {
+	if (window.innerWidth >= 600) {
+		$(document).on('mousemove', '.modal', function (event) {
+			var halfW = this.clientWidth / 2;
+			var halfH = this.clientHeight / 2;
+			var coorX = halfW - (event.screenX - this.offsetLeft);
+			var coorY = halfH - (event.screenY - this.offsetTop);
+			var degX = coorY / halfH * 6 + 'deg';
+			var degY = coorX / halfW * -6 + 'deg';
+
+			$(this).css('transform', function () {
+				return 'perspective(600px) translate3d(0, -2px, 0) scale(1.1) rotateX(' + degX + ') rotateY(' + degY + ')';
+			});
+		});
+	}
+});
+'use strict';
+
+$(function () {
   var introTop = $('#intro').position().top;
   var introMid = introTop + $('#intro').height() / 2;
   var introBottom = introTop + $('#intro').height();
