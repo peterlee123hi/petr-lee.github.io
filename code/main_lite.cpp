4d5adb174bd2d71c1f0cf120d3fe32528ffbd894
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-05-24 20:35:07 -0700

    Updated templates

--- code/main_lite.cpp
+++ code/main_lite.cpp
@@ -1,31 +1,9 @@
-#include <iostream>
-#include <sstream>
-#include <cstdio>
-#include <cmath>
-#include <cstring>
-#include <cctype>
-#include <string>
-#include <vector>
-#include <list>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <algorithm>
-#include <functional>
-using namespace std; 
-#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
-#define REP(i,n) for(int i=0;i<(n);i++)
-#define FOR(i,a,b) for(int i=(a);i<(b);i++)
-#define FORD(i,a,b) for(int i=(b)-1;i>=(a);i--)
-#define mp make_pair
+#include <bits/stdc++.h>
+using namespace std;
+#define mt make_tuple
 #define pb push_back
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(),(x).end()
-#define clr(x) memset((x),0,sizeof(x))
 #define fi first
 #define se second
-inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
 const int INF = 1<<29;
 typedef long long ll;
 typedef vector<int> vi;
